f0ebf1b007594335eb008c28ee47a727cae29a15
plochu <bartosz@it-ploch.pl>
2017-11-07 19:25:56 +0100

    wyznaczanie przejścia przez zero + obliczanie częstotliwości prądu

--- arduino-zero-crossing/arduino-zero-crossing.ino
+++ arduino-zero-crossing/arduino-zero-crossing.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+int i = 0;
+unsigned long CzasOne = 0;
+unsigned long CzasTwo = 0;
 
+void Zero()
+{
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  if ((millis() - CzasOne) <= 1000 ) {
+    i++;
+  }
+  else {
+     Serial.print("czestotliwosc pradu w sieci = ");
+     Serial.print(i/2);
+     Serial.println("Hz");
+    CzasOne = millis();
+    i = 0;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, INPUT);
+  attachInterrupt(0, Zero, FALLING);
+  CzasOne = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Serial.println(i);
 }
